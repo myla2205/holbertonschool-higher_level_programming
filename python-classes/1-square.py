5bd5da3802c9d0c084ad5b255d293addb098f7b2
myla2205 <myla2205@gmail.com>
2025-02-07 15:27:10 -0400

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,56 +5,44 @@
 class Square:
     """This class represents a Square."""
 
-    def __init__(self, width=0, height=0):
-        """Instantiation with optionals.
+    def __init__(self, size=0):
+        """Instantiation with an optional size.
 
         Args:
-            width (int, optional): The width of the square. Defaults to 0.
-            height (int, optional): The height of the square. Defaults to 0.
+            size (int, optional): The size of the square. Defaults to 0.
         """
-        self.width = width
-        self.height = height
+        self.size = size  # set both width and height
+
+    @property
+    def size(self):
+        """Function to retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Function to set the size of the square.
+
+        Args:
+            value (int): must be an int.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        self.__width = value  # Set width to the size value
+        self.__height = value  # Set height to the size value
 
     @property
     def width(self):
         """Function to retrieve the width of the square."""
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        """Function to set the width of the square.
-
-        Args:
-            value (int): must be an int.
-
-        Raises:
-            TypeError: if value is not an integer.
-            ValueError: if value < 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     @property
     def height(self):
-        """Function to retrieve the height of the square"""
+        """Function to retrieve the height of the square."""
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Function to set the height of the square.
-
-        Args:
-            value (int): must be an int.
-
-        Raises:
-            TypeError: if value is not an integer.
-            ValueError: if value < 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
