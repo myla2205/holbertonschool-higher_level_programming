f8afbc0b19ac61b91f669a589b656a12b737a74c
myla2205 <myla2205@gmail.com>
2025-02-07 15:55:17 -0400

    square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,48 +1,15 @@
 #!/usr/bin/python3
-"""Define a class named Square."""
+
+"""Defining a Class named Square."""
 
 
 class Square:
-    """This class represents a Square."""
+    """This class represents a square."""
 
-    def __init__(self, size=0):
-        """Instantiation with an optional size.
-
-        Args:
-            size (int, optional): The size of the square. Defaults to 0.
-        """
-        self.size = size  # set both width and height
-
-    @property
-    def size(self):
-        """Function to retrieve the size of the square."""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Function to set the size of the square.
+    def __init__(self, size):
+        """Initialize a square.
 
         Args:
-            value (int): must be an int.
-
-        Raises:
-            TypeError: if value is not an integer.
-            ValueError: if value < 0.
+            size (int): private instance attribute.
         """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-        self.__width = value  # Set width to the size value
-        self.__height = value  # Set height to the size value
-
-    @property
-    def width(self):
-        """Function to retrieve the width of the square."""
-        return self.__width
-
-    @property
-    def height(self):
-        """Function to retrieve the height of the square."""
-        return self.__height
+        self.__size = size
---
+++ python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""Define a class named Square."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size=0):
+        """Instantiation with optional
+
+        Args:
+            size (int, optional): Size of the square. Defaults to 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Function to retrieve the size of a square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """This function is to set the size of the square.
+
+        Args:
+            value (int): must be an integer.
+
+        Raises:
+            TypeError: if value is not an int.
+            ValueError: if value < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be an integer")
+        self.__size = value
+
+    def area(self):
+        """Function that gets the area of a square.
+
+        Returns:
+            int: the area of the square.
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Function that prints a # for the size of the square."""
+        for i in range(self.size):
+            print("#" * self.size)
+        if self.size == 0:
+            print()
