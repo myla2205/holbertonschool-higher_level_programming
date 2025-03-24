c5a3d04e63fa0f3d246598d5268031fce962a319
myla2205 <myla2205@gmail.com>
2025-03-24 14:49:03 -0400

    api

---
+++ restufl-api/task_02_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+
+def fetch_and_save_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if response.status_code == 200:
+        posts = response.json()
+        posts_data = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+        with open('posts.csv', "w", encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(posts_data)
