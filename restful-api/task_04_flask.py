84b3faa9934b252287ed708abfd676f212c3f889
myla2205 <myla2205@gmail.com>
2025-03-29 16:14:49 -0400

    04

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+users = {}
+
+
+@app.route("/")
+def home():
+    return 'Welcome to the Flask API!'
+
+
+@app.route('/data')
+def get_data():
+    return jsonify(list(users.keys()))
+
+
+@app.route('/status')
+def status():
+    return "OK"
+
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
