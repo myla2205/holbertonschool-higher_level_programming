9ba2e1d122b0fe26fb2306321d51d4afc4b63498
myla2205 <myla2205@gmail.com>
2025-04-13 14:43:16 -0400

    cero

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 
-'''my SQLldb'''
-
+"""
+Script that lists all states in the database.
+"""
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-        charset="utf8"
-    )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    query_rows = cur.fetchall()
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
 
-    for row in query_rows:
+    cur = db.cursor()
+    cur.execute("""SELECT id, name FROM states ORDER BY states.id ASC""")
+
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
     cur.close()
-    conn.close()
+    db.close()
