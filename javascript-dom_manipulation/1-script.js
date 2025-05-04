0157bbd928e9d9fe3a6ec60fef943413b83edac3
myla2205 <myla2205@gmail.com>
2025-05-04 13:39:33 -0400

    asd

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+// Script that updates the text color of the header element to red when the user clicks on the tag with id red_header
+document.getElementById('red_header').addEventListener('click', function () {
+    document.querySelector('header').style.color = '#FF0000';
+  });ls
+  
